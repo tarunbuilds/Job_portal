08e9c4d12ddb34a2fd111256edf683367351f452
lucky-727 <luckygarg727@gmail.com>
2026-01-02 21:14:56 +0530

    cors origin update for deployment on render

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://job-portal-40kc.onrender.com"],
     credentials: true,
   })
 );
